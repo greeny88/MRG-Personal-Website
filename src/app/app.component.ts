52a44ca007b2d6d938e0cb5b4d9b57e9983d2834
Matthew Green <greeny88@gmail.com>
2026-09-07 21:52:59 -0500

    Changed app bootstrap and remove last module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { MatToolbar } from '@angular/material/toolbar';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { MatIcon } from '@angular/material/icon';
+import { MatSidenavContainer, MatSidenav, MatSidenavContent } from '@angular/material/sidenav';
+import { MatNavList, MatListItem } from '@angular/material/list';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
-    standalone: false
+    imports: [MatToolbar, RouterLink, RouterLinkActive, MatIcon, MatSidenavContainer, MatSidenav, MatNavList, MatListItem, MatSidenavContent, RouterOutlet]
 })
 export class AppComponent {
 }
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,17 @@
-import { platformBrowser } from '@angular/platform-browser';
-
-import { AppModule } from './app/app.module';
+import { platformBrowser, BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 
 
-platformBrowser().bootstrapModule(AppModule)
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { AppRoutingModule } from './app/app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppComponent } from './app/app.component';
+import { importProvidersFrom } from '@angular/core';
+
+
+bootstrapApplication(AppComponent, {
+    providers: [importProvidersFrom(BrowserModule, MatIconModule, MatListModule, MatSidenavModule, MatToolbarModule, AppRoutingModule, BrowserAnimationsModule)]
+})
   .catch(err => console.error(err));
